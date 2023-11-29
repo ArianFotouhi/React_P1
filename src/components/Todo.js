734ed7d53e2c4a6e214afdf9fcf81fa9a606f182
ArianFotouhi <fotouhiarian@gmail.com>
2023-11-29 17:06:58 -0500

    v1.4

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import logo from '../logo.svg';
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler(){
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+
+      <div className='actions'>
+        <button className='btn' onClick={deleteHandler}>Delete</button>
+      </div>
+
+      {modalIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null}
+      {modalIsOpen ? <Backdrop onCancel={closeModalHandler}/> : null}
+
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+    </div>
+  );
+}
+
+export default Todo;
+
