a83d4385503379a237566581e2beb9b0723edecc
ArianFotouhi <fotouhiarian@gmail.com>
2023-11-30 10:34:52 -0500

    v1.6

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css'
 import { useRef } from 'react';
 
-function NewMeetupForm(){
+function NewMeetupForm(props){
     //to read the form (title)
     const titleInputRef = useRef();
     const imageInputRef = useRef();
@@ -24,7 +24,8 @@ function NewMeetupForm(){
             address: enteredAddress,
             description: enteredDescription
         }
-        console.log(meetupData);
+       
+        props.onAddMeetup(meetupData);
     }
 
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(){
+
+    function addMeetupHandler(meetupData){
+        /// /meetups.json creates a table named meetups (it's a firebase feature)
+        fetch(
+            'https://react-meetups-bc0bf-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                header: {
+                    'Content-Type':'application/json'
+                }
+            }
+         );
+
+    }
+
     return <section>
     <h1>Add New Meetup</h1>
-    <NewMeetupForm/>
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
 
