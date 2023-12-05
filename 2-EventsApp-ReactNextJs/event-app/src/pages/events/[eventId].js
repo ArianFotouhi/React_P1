04f540ccdce4137bae57ecb0b5f9b3a8a2e0ecea
ArianFotouhi <fotouhiarian@gmail.com>
2023-12-05 17:11:52 -0500

    v2.0.0

--- 2-EventsApp-ReactNextJs/event-app/src/pages/events/[eventId].js
+++ 2-EventsApp-ReactNextJs/event-app/src/pages/events/[eventId].js
@@ -1,9 +1,24 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../../dummy-data";
+
+
+
 function EventDetailPage(){
-return <div>
+const router = useRouter();
+const eventId = router.query.eventId;
+
+const event = getEventById(eventId);
+
+if(!event){
+return <p>No event found!</p>;
+}
+return (
+<div>
     <h1>
         Event Detail
     </h1>
 </div>
+        );
 }
 
 export default EventDetailPage;
