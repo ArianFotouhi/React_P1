c9d9c3bac21d2d10a3e13e70cba8b27d3fbc6d89
ArianFotouhi <fotouhiarian@gmail.com>
2023-12-06 15:30:02 -0500

    v2.0.1

---
+++ 2-EventsApp-ReactNextJs/event-app/components/layout/main-header.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import classes from './main-header.module.css';
+
+export default function MainHeader(){
+return <header className={classes.header}>
+            <div className={classes.logo}>
+                <Link href = '/'>Next Events</Link>
+            </div> 
+            <nav className={classes.navigation}>
+                <ul className={classes.ul}>
+                    <li className={classes.ul_li}>
+                        <Link href='/events'>All Events</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+}
--- 2-EventsApp-ReactNextJs/event-app/src/pages/events/[eventId].js
+++ 2-EventsApp-ReactNextJs/event-app/src/pages/events/[eventId].js
@@ -1,5 +1,9 @@
 import { useRouter } from "next/router";
 import { getEventById } from "../../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../../components/event-detail/event-summary";
+import EventLogistics from "../../../components/event-detail/event-logistics";
+import EventContent from "../../../components/event-detail/event-content";
 
 
 
@@ -13,11 +17,17 @@ if(!event){
 return <p>No event found!</p>;
 }
 return (
-<div>
-    <h1>
-        Event Detail
-    </h1>
-</div>
+<Fragment>
+    <EventSummary title = {event.title}/>
+    <EventLogistics date={event.date} 
+                    address={event.location} 
+                    image={event.image}
+                    imageAlt={event.title}
+                        />
+    <EventContent>
+        <p>{event.description}</p>
+    </EventContent>
+</Fragment>
         );
 }
 
