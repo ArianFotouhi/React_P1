84462e871c6b24ef88adb1106795b397efa92508
ArianFotouhi <fotouhiarian@gmail.com>
2023-12-06 16:37:14 -0500

    v2.0.3

--- 2-EventsApp-ReactNextJs/event-app/components/events/events-search.js
+++ 2-EventsApp-ReactNextJs/event-app/components/events/events-search.js
@@ -1,19 +1,33 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import classes from './events-search.module.css';
 
+
 export default function EventsSearch(props){
-    return  (<form className={classes.form}> 
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    function submitHandler(event){
+        event.preventDefault(); 
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+
+        props.onSearch(selectedYear, selectedMonth);
+    }
+
+    return  (
+            <form className={classes.form} onSubmit={submitHandler}> 
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor="year">Year</label>
-                        <select id="year">
+                        <select id="year" ref={yearInputRef}>
                             <option value="2021">2021</option>
                             <option value="2022">2022</option>
                         </select>
                     </div>
                     <div className={classes.control}>
                         <label htmlFor="month">Month</label>
-                        <select id='month'>
+                        <select id='month' ref={monthInputRef}>
                             <option value='1'>Jan</option>
                             <option value='2'>Feb</option>
                             <option value='3'>Mar</option>
@@ -27,9 +41,9 @@ export default function EventsSearch(props){
                             <option value='11'>Nov</option>
                             <option value='12'>Dec</option>
                         </select>
-
                     </div>
                 </div>
                 <Button>Find Events</Button>
-            </form>)
+            </form>
+            )
 }
